7d7036d6b806785221c26a4eeb5337f0dba34c68
du khon <32060434+benzi96@users.noreply.github.com>
2018-03-17 17:51:32 +0700

    v3.1
    test

---
+++ CookingRecipes/app/src/main/java/com/assignment/pc/cookingrecipes/Items.java
@@ -0,0 +1,46 @@
+package com.assignment.pc.cookingrecipes;
+
+
+class Items {
+
+    String image,title,link,caption;
+
+    public Items(String image, String title, String link, String caption) {
+        this.image = image;
+        this.title = title;
+        this.link = link;
+        this.caption = caption;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
